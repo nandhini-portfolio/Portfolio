f1bd9bbbabe7f47555787d6e4536397ba736dacd
nandhini-portfolio <nandhiniweb64@gmail.com>
2026-06-17 10:04:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Website Developer", "Website Designer"],
+        strings: ["Front-End Developer", "Wordpress Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Website Developer", "Website Designer"],
+        strings: ["Front-End Developer", "Wordpress Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
